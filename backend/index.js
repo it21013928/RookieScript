9d8cc21689adb4762f925bc866b7ed965fe57f79
Kithmina Siriwardana <it21020230@my.sliit.lk>
2023-09-05 11:29:51 +0530

    MongoDB connection added

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,37 @@
 require("dotenv").config();
 
-const port = process.env.PORT;
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT;
 const app = express();
 
 // middleware
 app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+
 app.use(cors());
 
+//Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error.message);
+  });
+
 app.listen(port, () => {
-  console.log(
-    `Connected to the Database and Listening on http://localhost:${port}`
-  );
+  console.log(`Server is listening on http://localhost:${port}`);
 });
 
 module.exports = app;
