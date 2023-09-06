b1ab6a25190ec50d3137e03231537466eb3bd367
Kithmina Siriwardana <it21020230@my.sliit.lk>
2023-09-06 12:41:05 +0530

    lessonController n lessonRoutes updated

--- backend/src/routes/lessonRoutes.js
+++ backend/src/routes/lessonRoutes.js
@@ -5,6 +5,11 @@ const {
   createLesson,
   getAllLessons,
   getLessonById,
+  getLessonsByXpLevel,
+  getLessonsByLanguage,
+  getLessonsWithVideos,
+  getLessonsWithArticles,
+  getLessonsWithQuizzes,
   updateLesson,
   deleteLesson,
 } = require("../controllers/lessonController");
@@ -17,6 +22,21 @@ router.get("/", getAllLessons);
 
 //Get lesson by id
 router.get("/:id", getLessonById);
+
+//Get lessons by xpLevel
+router.get("/xpLevel/:xpLevel", getLessonsByXpLevel);
+
+//Get lessons by language
+router.get("/language/:language", getLessonsByLanguage);
+
+//Get lessons with videos
+router.get("/videos", getLessonsWithVideos);
+
+//Get lessons with articles
+router.get("/articles", getLessonsWithArticles);
+
+//Get lessons with quizzes
+router.get("/quizzes", getLessonsWithQuizzes);
 
 //Update a lesson
 router.patch("/:id", updateLesson);
